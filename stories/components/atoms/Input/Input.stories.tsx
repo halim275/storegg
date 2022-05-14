3c3001330106311f7c2b1383814e8b7cf2522799
halim275 <halim.devlop@gmail.com>
2022-05-14 10:14:16 +0700

    Input Stories

---
+++ stories/components/atoms/Input/Input.stories.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { Meta } from '@storybook/react';
+import Input, { InputProps } from '../../../../components/atoms/Input';
+
+export default {
+  title: 'Components/Atoms/Input',
+  component: Input,
+} as Meta;
+
+function Template(args: InputProps) {
+  return <Input {...args} />;
+}
+
+export const Default = Template.bind({});
+Default.args = {
+  label: 'Nama Lengkap',
+};
