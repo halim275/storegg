a3a57a20dec175af2d2838aa3b5fa1e64c9544ca
halim275 <halim.devlop@gmail.com>
2022-05-14 09:28:07 +0700

    Beres Refactor

--- components/organisms/SideBar/MenuItem.tsx
+++ components/organisms/SideBar/MenuItem.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable object-curly-newline */
 import Image from 'next/image';
 import Link from 'next/link';
 import cx from 'classnames';
@@ -6,9 +7,10 @@ interface MenuItemProps {
   title: string;
   icon: 'ic-menu-overview' | 'ic-menu-card' | 'ic-menu-logout' | 'ic-menu-messages' | 'ic-menu-rewards' | 'ic-menu-settings' | 'ic-menu-transaction';
   active?: boolean;
+  href: string;
 }
 export default function MenuItem(props: Partial<MenuItemProps>) {
-  const { title, icon, active } = props;
+  const { title, icon, active, href } = props;
   const classItem = cx({
     item: true,
     'mb-30': true,
@@ -20,7 +22,7 @@ export default function MenuItem(props: Partial<MenuItemProps>) {
         <Image src={`/icon/${icon}.svg`} width={25} height={25} alt="ic-menu" />
       </div>
       <p className="item-title m-0">
-        <Link href="/member">
+        <Link href={href}>
           <a className="text-lg text-decoration-none">{title}</a>
         </Link>
       </p>
--- components/organisms/TransactionContent/TableRow.tsx
+++ components/organisms/TransactionContent/TableRow.tsx
@@ -1,16 +1,17 @@
+import Link from 'next/link';
 import cx from 'classnames';
 
 interface TableRowProps {
+  image: string;
   title: string;
-  categori: string;
+  category: string;
   item: number;
   price: number;
   status: 'Pending' | 'Success' | 'Failed';
-  image: string;
 }
 
 export default function TableRow(props: TableRowProps) {
-  const { title, categori, item, price, status, image } = props;
+  const { image, title, category, item, price, status } = props;
   const classStatus = cx({
     'float-start icon-status': true,
     pending: status === 'Pending',
@@ -18,25 +19,31 @@ export default function TableRow(props: TableRowProps) {
     failed: status === 'Failed',
   });
   return (
-    <tr className="align-middle">
+    <tr data-category="pending" className="align-middle">
       <th scope="row">
-        <img className="float-start me-3 mb-lg-0 mb-3" src={`/img/${image}.png`} width={80} height={60} alt="Icon" />
+        <img className="float-start me-3 mb-lg-0 mb-3" src={`/img/${image}.png`} width="80" height="60" alt="" />
         <div className="game-title-header">
           <p className="game-title fw-medium text-start color-palette-1 m-0">{title}</p>
-          <p className="text-xs fw-normal text-start color-palette-2 m-0">{categori}</p>
+          {/* Mobile Legends: The New Battle 2021 */}
+          <p className="text-xs fw-normal text-start color-palette-2 m-0">{category}</p>
         </div>
       </th>
       <td>
-        <p className="fw-medium color-palette-1 m-0">${item} Gold</p>
+        <p className="fw-medium color-palette-1 m-0">{item} Gold</p>
       </td>
       <td>
-        <p className="fw-medium text-start color-palette-1 m-0">{price}</p>
+        <p className="fw-medium color-palette-1 m-0">{price}</p>
       </td>
       <td>
         <div>
           <span className={classStatus} />
           <p className="fw-medium text-start color-palette-1 m-0 position-relative">{status}</p>
         </div>
+      </td>
+      <td>
+        <Link href="/member/transactions/detail">
+          <a className="btn btn-status rounded-pill text-sm">Details</a>
+        </Link>
       </td>
     </tr>
   );
